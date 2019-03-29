6b946f543435052071948ec15998fa6752531bc1
franbuehler <franziska.buehler.schmocker@gmail.com>
2019-03-29 13:59:58 +0100

    Added little example test

---
+++ tests/testcrs.js
@@ -0,0 +1,13 @@
+import { Selector } from 'testcafe';
+
+fixture`Getting Started`
+    .page `http://172.0.0.1:8001/register`;
+
+test('Register User WITH CRS', async t => {
+    await t
+        .typeText('input#email', 'testuser@pixi')
+        .typeText('input#password', 'testpw')
+        .click('body > div.jumbotron.jumbotron-fluid > div > div:nth-child(5) > div.col-8 > form > button')
+        //After registration the search pixi field should be there
+        //.expect('#search_query')
+});
